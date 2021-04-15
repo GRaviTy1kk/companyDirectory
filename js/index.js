ab2e0a92148ae5db001c1aad13cb9cab5b86a800
GRaviTy1kk <72166090+GRaviTy1kk@users.noreply.github.com>
2021-04-15 03:13:10 +0100

    styling1

--- js/index.js
+++ js/index.js
@@ -1,12 +1,7 @@
 //onload operations
 $(window).on('load', function() {
     
-    //preloader
-    if ($('#preloader').length) {
-        $('#preloader').delay(100).fadeOut('slow', function () {
-            $(this).remove();
-        });
-    }
+    
 
 });
 
